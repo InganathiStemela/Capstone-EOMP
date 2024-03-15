d82ee1df84ef6ce4741a00cb7cd2d23ea486f353
InganathiStemela <inganathistemela13@gmail.com>
2024-03-15 11:39:41 +0200

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import path from 'path';
 
 const app = express()
-const port = +process.env.PORT || 6300
+const port = +process.env.PORT || 3000
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/');
